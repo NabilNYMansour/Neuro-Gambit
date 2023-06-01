8a5fc94a7828fb69ee4e7dfa71083ec5dbdb3478
Nabil Mansour <nabilmansour1999@gmail.com>
2023-06-01 18:10:46 -0400

    new stuff

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import chess
-from chess_transformation import board_to_matrix, matrix_to_linear
+from chess_transformation import linear_to_matrix, matrix_to_board, board_to_matrix, matrix_to_linear, uci_to_alg
+import torch.nn.functional as F
 
 def one_hot_encode_labels(labels : list[str]):
     labels_dict = {'.': 0, 'K': 1, 'Q': 2, 'B': 3, 'N': 4, 'R': 5, 'P': 6, 'k': 7, 'q': 8, 'b': 9, 'n': 10, 'r': 11, 'p': 12}
@@ -14,6 +15,8 @@ def encode_player_col(player : str):
     player_col = torch.Tensor([1 if player == 'white' else 0])
     return player_col
 
+
+# AIs
 class Neuro_gambit(nn.Module):
     def __init__(self):
         super(Neuro_gambit, self).__init__()
@@ -48,11 +51,94 @@ class Neuro_gambit(nn.Module):
     def forward_chess_board_input(self, board : chess.Board, player : str):
         positions = matrix_to_linear(board_to_matrix(board))
         return self.forward_str_input(positions, player)
-    
-import torch.nn.functional as F
 
-import chess
-from chess_transformation import linear_to_matrix, matrix_to_board, uci_to_alg
+class Neuro_gambit_2(nn.Module):
+    def __init__(self):
+        super(Neuro_gambit_2, self).__init__()
+        # self.lin_test = nn.Linear(833, 36) # input 64*13 + 1, output 36
+        self.encoder = nn.Sequential(
+            nn.Linear(833, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU()
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 36)
+        )
+
+    def forward_str_input(self, positions : list[str], player : str):
+        pos_tensor = one_hot_encode_labels(positions)
+        player_col = encode_player_col(player)
+        device = next(self.parameters()).device
+        input_layer = torch.cat((pos_tensor, player_col.unsqueeze(1)), dim=1).to(device)
+        output = self.decoder(self.encoder(input_layer))
+        return torch.split(output, [8, 8, 8, 8, 4], dim=1)
+
+    def forward(self, x : torch.Tensor):
+        output = self.decoder(self.encoder(x))
+        return torch.split(output, [8, 8, 8, 8, 4], dim=1)
+    
+    def forward_chess_board_input(self, board : chess.Board, player : str):
+        positions = matrix_to_linear(board_to_matrix(board))
+        return self.forward_str_input(positions, player)
+
+
+class Neuro_gambit_3(nn.Module):
+    def __init__(self):
+        super(Neuro_gambit_3, self).__init__()
+        # self.lin_test = nn.Linear(833, 36) # input 64*13 + 1, output 36
+        self.encoder = nn.Sequential(
+            nn.Linear(833, 8192),
+            nn.ReLU(),
+            nn.Linear(8192, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 2048),
+            nn.ReLU()
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(2048, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 8192),
+            nn.ReLU(),
+            nn.Linear(8192, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 36),
+        )
+
+    def forward_str_input(self, positions : list[str], player : str):
+        pos_tensor = one_hot_encode_labels(positions)
+        player_col = encode_player_col(player)
+        device = next(self.parameters()).device
+        input_layer = torch.cat((pos_tensor, player_col.unsqueeze(1)), dim=1).to(device)
+        output = self.decoder(self.encoder(input_layer))
+        return torch.split(output, [8, 8, 8, 8, 4], dim=1)
+
+    def forward(self, x : torch.Tensor):
+        output = self.decoder(self.encoder(x))
+        return torch.split(output, [8, 8, 8, 8, 4], dim=1)
+    
+    def forward_chess_board_input(self, board : chess.Board, player : str):
+        positions = matrix_to_linear(board_to_matrix(board))
+        return self.forward_str_input(positions, player)
+
 
 def get_all_possible_moves(board : chess.Board, player : str):
     possible_moves = []
